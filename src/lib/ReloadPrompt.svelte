1f03c1cd1fdfec8984b847cd37b5461871863367
Max Sommer <max.sommer@bevelop.de>
2023-01-21 17:16:02 +0100

    fix: reload prompt

--- src/lib/ReloadPrompt.svelte
+++ src/lib/ReloadPrompt.svelte
@@ -4,10 +4,11 @@
 	const { needRefresh, updateServiceWorker, offlineReady } = useRegisterSW({
 		onRegistered(r: any) {
 			// uncomment following code if you want check for updates
-			// r && setInterval(() => {
-			//    console.log('Checking for sw update')
-			//    r.update()
-			// }, 20000 /* 20s for testing purposes */)
+			r &&
+				setInterval(() => {
+					console.log('Checking for sw update');
+					r.update();
+				}, 20000 /* 20s for testing purposes */);
 			console.log(`SW Registered: ${r}`);
 		},
 		onRegisterError(error: any) {
@@ -25,9 +26,9 @@
 	<div class="pwa-toast" role="alert">
 		<div class="message">
 			{#if $offlineReady}
-				<span> App ready to work offline </span>
+				<span>App ready to work offline</span>
 			{:else}
-				<span> New content available, click on reload button to update. </span>
+				<span>New content available, click on reload button to update.</span>
 			{/if}
 		</div>
 		{#if $needRefresh}
